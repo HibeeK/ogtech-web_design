21dc063b7da2188242b41151144aecb6f3310079
HibeeK <107434897+HibeeK@users.noreply.github.com>
2022-12-13 15:59:41 +0100

    new

--- 0x02-javascript/js/script.js
+++ 0x02-javascript/js/script.js
@@ -4,8 +4,22 @@
 // }
 
 // declaring variables:
+// let a = 1;
+// let b = 5;
+// let result = a+b;
+// alert("The result of 1+5 is " + result);
+
+// JS Datatypes
+// Conditional Statements
 let a = 1;
-let b = 5;
-let result = a+b;
-alert("The result of 1+5 is " + result);
+let b = 30;
+let name = 'great';
+let num = true;
+
+if (a>b){
+	document.write('A is bigger than b')
+}
+else {
+	document.write('A is lesser than b');
+}
  
