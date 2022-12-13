994954210f82013a7f60253ab2bd9ccf84fe37c7
HibeeK <107434897+HibeeK@users.noreply.github.com>
2022-12-13 15:47:54 +0100

    new

--- 0x02-javascript/js/script.js
+++ 0x02-javascript/js/script.js
@@ -1,1 +1,11 @@
-alert("This is JS");
+// alert("This is JS");
+// function Trigger(){
+// 	alert("Hibee");
+// }
+
+// declaring variables:
+let a = 1;
+let b = 5;
+let result = a+b;
+alert("The result of 1+5 is " + result);
+ 
